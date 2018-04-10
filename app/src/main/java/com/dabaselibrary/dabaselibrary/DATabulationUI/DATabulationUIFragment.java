4a2072c51142e135638e231aa280083202bbfaa6
DA_niao <842043648@qq.com>
2018-04-10 11:10:41 +0800

    ..

---
+++ app/src/main/java/com/dabaselibrary/dabaselibrary/DATabulationUI/DATabulationUIFragment.java
@@ -0,0 +1,18 @@
+package com.dabaselibrary.dabaselibrary.DATabulationUI;
+
+import android.view.View;
+
+import com.dabaselibrary.dabaselibrary.DABase.DABaseFragment;
+
+/**
+ * Created by DA on 2017/09/01.
+ * 下拉刷新和上拉加载更多的一个Fragment封装
+ */
+
+public class DATabulationUIFragment extends DABaseFragment{
+
+    @Override
+    protected View initView() {
+        return null;
+    }
+}
